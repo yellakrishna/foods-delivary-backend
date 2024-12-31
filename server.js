e2fb67cf2173386e2b29a324d2862808ff3e6977
yellakrishna <yellakrishna7675@gmail.com>
2024-12-31 15:30:07 +0530

    add backend

---
+++ server.js
@@ -0,0 +1,53 @@
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
+
+// app.use(cors({
+//     origin:"https://online-delivery-frontend.vercel.app",
+//     credentials:true
+// }))
+
+// middlewares
+app.use(express.json())
+
+const allowedOrigins = [
+    "https://online-delivery-frontend.vercel.app",
+    "https://online-delivery-admin.vercel.app"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
