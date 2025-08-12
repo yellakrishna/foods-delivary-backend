38180a58a285f59af4b2112f2cea9d1edf286730
yellakrishna <yellakrishna7675@gmail.com>
2025-08-12 16:35:34 +0530

    main

--- server.js
+++ server.js
@@ -27,10 +27,8 @@ connectDB();
 
 
 const allowedOrigins = [
-  "http://localhost:5173", // User frontend
-  "http://localhost:5174", // Admin frontend
-  "https://new-online-delivery-frontend.vercel.app",
-  "https://new-admin-gray.vercel.app"
+ "https://frontend-food-ai.vercel.app",
+"https://admin-food-ai.vercel.app"
 ];
 
 app.use(cors({
